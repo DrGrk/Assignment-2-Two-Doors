c660a14c4cc910dfac3220fa72ff8dc68ff2689f
David Guichon <davidrguichon@gmail.com>
2017-02-06 19:38:20 -0500

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by David Guichon on 2017-02-06.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
